af59863228e9d1829d8b10b89bd9546f4df2daad
Supriya <supriyaaa.926@gmail.com>
2026-01-23 10:20:24 +0530

    Learning variables

--- fsd.js
+++ fsd.js
@@ -1,2 +1,4 @@
 console.log("Hello from fsd.js");
 console.log("This is a sample file for demonstration purposes.");
+data_type Variable = "Sample Data";  //C,C++,Java
+// 'Var 'Let 'Const - JavaScript, TypeScript
