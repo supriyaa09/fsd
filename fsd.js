32343b62e5ad5bc8bdc1bae45dc453c60c33d868
Supriya <supriyaaa.926@gmail.com>
2026-01-23 10:23:45 +0530

    FSD

--- fsd.js
+++ fsd.js
@@ -1,4 +1,15 @@
 console.log("Hello from fsd.js");
 console.log("This is a sample file for demonstration purposes.");
-data_type Variable = "Sample Data";  //C,C++,Java
+//data_type Variable = "Sample Data";  //C,C++,Java
 // 'Var 'Let 'Const - JavaScript, TypeScript
+var firstName = "John";  // String
+let age = 30;           // Number
+const isStudent = false; // Boolean
+
+firstName = "Jane";
+age = 25;
+// isStudent = true; // This will cause an error
+
+console.log("Name:", firstName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
